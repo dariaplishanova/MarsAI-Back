4afc886402a581abeca6478b538a4f81953b512d
Elyas B. <elyas.benyoub@laplateforme.io>
2026-02-02 13:57:49 +0100

    Remove file extension from movie controller import

--- src/routes/movie.router.ts
+++ src/routes/movie.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import movieController from '../controllers/movie.controller.js';
+import movieController from '../controllers/movie.controller';
 
 const router = express.Router();
 
